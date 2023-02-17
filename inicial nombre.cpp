3bcbe55dff613d47d2807aa1f1083600ccb4e589
HuberCAPP <capperezhuber@cticoban.edu.gt>
2023-02-16 22:49:55 -0600

    agregar

---
+++ inicial nombre.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){ 
+    char nombre [0];
+       
+    cout<<"Ingrese su nombre: ";
+    cin>>nombre;
+      
+    cout<<"Hola" <<nombre<<" Su Nombre Comienza Con La Letra: "<<nombre[0]<<endl;
+       
+    return 0;
+
+	
+}
